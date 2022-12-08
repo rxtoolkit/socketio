19b7ec92ecc0c33fcde57ed6d7c8604b4f555576
Matthew Aho <ahomatthew@gmail.com>
2022-12-08 11:19:02 -0500

    feat: THR-266 Reconnect when wifi cuts out on microphone and resume stream

--- src/creators/io.js
+++ src/creators/io.js
@@ -69,6 +69,12 @@ function createObservableFromSocket(
     const emitToTopic = createTopicEmitter(obs);
     topics.map(handleTopic(client, obs, emitToTopic));
     client.on('error', error => obs.error(error));
+    client.io.on('reconnect', () => {
+      const obj = eventHandlerMap.reconnect(1);
+      console.log('socket.io.on("reconnect") firing');
+      console.log('calling obs.next(obj) with', obj);
+      return obs.next(obj);
+    });
   });
 }
 
@@ -85,7 +91,9 @@ const io = function io({
   const client = _socketIO(url, {...defaultOptions, ...socketOptions});
   const action$ = _createObservableFromSocket(client, topics);
   return action$.pipe(
-    map(action => [client, action]),
+    map(action => {
+      return [client, action];
+    }),
     takeUntil(stop$.pipe(
       tap(() => client.disconnect()) // instruct client to disconnect
     )),
